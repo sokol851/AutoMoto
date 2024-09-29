0b4ac0fea26ca1ade55a96e0c2c4bd4c901cac02
Ilia Akatov <ilia-akatoff@ya.ru>
2024-09-29 17:22:11 +0300

    settings apps

--- config/settings.py
+++ config/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+from datetime import timedelta
 from pathlib import Path
 from decouple import config
 
@@ -169,4 +169,15 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
+    # 'DEFAULT_PERMISSION_CLASSES': (
+    #     'rest_framework.permissions.IsAuthenticated',
+    # ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.AllowAny',
+    ),
+}
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=15),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
 }
--- vehicle/admin.py
+++ vehicle/admin.py
@@ -1,8 +1,18 @@
 from django.contrib import admin
 
-from vehicle.models import Car
+from vehicle.models import Car, Moto, Milage
 
 
 @admin.register(Car)
-class BlogAdmin(admin.ModelAdmin):
-    list_display = ('id', 'title', 'description')
+class CarAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'description',)
+
+
+@admin.register(Moto)
+class MotoAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'description',)
+
+
+@admin.register(Milage)
+class MilageAdmin(admin.ModelAdmin):
+    list_display = ('id', 'car', 'moto', 'milage', 'year',)
