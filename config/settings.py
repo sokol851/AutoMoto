1f872468c0acd39efe64126604fc456721e4d0bb
Ilia Akatov <ilia-akatoff@ya.ru>
2024-10-20 15:05:58 +0300

    Added celery in project

--- config/settings.py
+++ config/settings.py
@@ -197,3 +197,21 @@ CORS_ALLOW_ALL_ORIGINS = False
 
 CUR_API_URL = config('CUR_API_URL')
 CUR_API_KEY = config('CUR_API_KEY')
+
+
+# Настройки для Celery
+
+# URL-адрес брокера сообщений
+CELERY_BROKER_URL = 'redis://localhost:6379'  # Например, Redis, который по умолчанию работает на порту 6379
+
+# URL-адрес брокера результатов, также Redis
+CELERY_RESULT_BACKEND = 'redis://localhost:6379'
+
+# Часовой пояс для работы Celery
+CELERY_TIMEZONE = "Europe/Moscow"
+
+# Флаг отслеживания выполнения задач
+CELERY_TASK_TRACK_STARTED = True
+
+# Максимальное время на выполнение задачи
+CELERY_TASK_TIME_LIMIT = 30 * 60
