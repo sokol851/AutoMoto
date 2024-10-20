859d6eabe9fddf69e2d4e4216738999b85702b95
Ilia Akatov <ilia-akatoff@ya.ru>
2024-10-20 15:28:11 +0300

    Added task send message if mileage broken

--- config/settings.py
+++ config/settings.py
@@ -202,16 +202,16 @@ CUR_API_KEY = config('CUR_API_KEY')
 # Настройки для Celery
 
 # URL-адрес брокера сообщений
-CELERY_BROKER_URL = 'redis://localhost:6379'  # Например, Redis, который по умолчанию работает на порту 6379
+CELERY_BROKER_URL = 'redis://localhost:6379/0'  # Например, Redis, который по умолчанию работает на порту 6379
 
 # URL-адрес брокера результатов, также Redis
-CELERY_RESULT_BACKEND = 'redis://localhost:6379'
+CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
 
-# Часовой пояс для работы Celery
-CELERY_TIMEZONE = "Europe/Moscow"
+# # Часовой пояс для работы Celery
+# CELERY_TIMEZONE = "Europe/Moscow"
 
-# Флаг отслеживания выполнения задач
-CELERY_TASK_TRACK_STARTED = True
-
-# Максимальное время на выполнение задачи
-CELERY_TASK_TIME_LIMIT = 30 * 60
+# # Флаг отслеживания выполнения задач
+# CELERY_TASK_TRACK_STARTED = True
+#
+# # Максимальное время на выполнение задачи
+# CELERY_TASK_TIME_LIMIT = 30 * 60
